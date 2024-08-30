8af464c15e88f3d6c9adb4fa4ad7273168b663b8
rutvikkshirsagar14 <rutvikkshirsagar14@gmail.com>
2024-08-30 14:21:38 +0530

    Add files via upload

---
+++ MarkCalculator.java
@@ -0,0 +1,54 @@
+import java.util.Scanner;
+
+public class MarkCalculator
+ {
+    public static void main(String[] args)
+     {
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.print("Enter the number of subjects: ");
+        int numSubjects = sc.nextInt();
+        
+        int[] marks = new int[numSubjects];
+        int totalMarks = 0;
+        
+        for (int i = 0; i < numSubjects; i++)
+        {
+            System.out.print("Enter marks for subject " + (i + 1) + ": ");
+            marks[i] = sc.nextInt();
+            totalMarks += marks[i];
+        }
+        
+        double averagePercentage = (double) totalMarks / numSubjects;
+
+        char grade;
+        if (averagePercentage >= 90)
+         {
+            grade = 'A';
+         } 
+         else if (averagePercentage >= 80)
+         {
+            grade = 'B';
+         } 
+          else if (averagePercentage >= 70)
+         {
+            grade = 'C';
+         }
+         else if (averagePercentage >= 60)
+         {
+            grade = 'D';
+         }
+         else
+         {
+            grade = 'F';
+         }
+        
+        // Display the results
+        System.out.println("\nResults:");
+        System.out.println("Total Marks: " + totalMarks);
+        System.out.println("Average Percentage: " + averagePercentage + "%");
+        System.out.println("Grade: " + grade);
+        
+        sc.close();
+    }
+}
